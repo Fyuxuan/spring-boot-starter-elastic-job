30dd92792220a91c9e8cdc2e5449b589748b7544
yuxuan <1412858283@qq.com>
2018-09-17 10:14:39 +0800

    commit

---
+++ src/main/java/com/yuxuan/elasticjob/spring/boot/RegistryCenterAutoConfiguration.java
@@ -0,0 +1,45 @@
+package com.yuxuan.elasticjob.spring.boot;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.dangdang.ddframe.job.api.ElasticJob;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import com.yuxuan.elasticjob.spring.boot.annotaion.ElasticJobConfig;
+
+/**
+ * 注册中心
+ *
+ * @author yuxuan
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+@Configuration
+@ConditionalOnClass(ElasticJob.class)
+@ConditionalOnBean(annotation = ElasticJobConfig.class)
+@EnableConfigurationProperties(ZookeeperRegistryProperties.class)
+public class RegistryCenterAutoConfiguration {
+
+    @Autowired
+    private ZookeeperRegistryProperties regCenterProperties;
+
+    @Bean(initMethod = "init")
+    @ConditionalOnMissingBean
+    public ZookeeperRegistryCenter regCenter() {
+        ZookeeperConfiguration zookeeperConfiguration = new ZookeeperConfiguration(regCenterProperties.getServerLists(), regCenterProperties.getNamespace());
+        zookeeperConfiguration.setBaseSleepTimeMilliseconds(regCenterProperties.getBaseSleepTimeMilliseconds());
+        zookeeperConfiguration.setConnectionTimeoutMilliseconds(regCenterProperties.getConnectionTimeoutMilliseconds());
+        zookeeperConfiguration.setMaxSleepTimeMilliseconds(regCenterProperties.getMaxSleepTimeMilliseconds());
+        zookeeperConfiguration.setSessionTimeoutMilliseconds(regCenterProperties.getSessionTimeoutMilliseconds());
+        zookeeperConfiguration.setMaxRetries(regCenterProperties.getMaxRetries());
+        zookeeperConfiguration.setDigest(regCenterProperties.getDigest());
+        return new ZookeeperRegistryCenter(zookeeperConfiguration);
+    }
+
+}
